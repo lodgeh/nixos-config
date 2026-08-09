d79c399ae5b0bde86a9400a90dc25a2a2d5bfe55
lodgeh <19654724+lodgeh@users.noreply.github.com>
2026-08-09 23:25:45 +0100

    make domain https

--- modules/homelab/services/vaultwarden/default.nix
+++ modules/homelab/services/vaultwarden/default.nix
@@ -26,7 +26,7 @@ in
     services.vaultwarden = {
       enable = true;
       config = {
-        DOMAIN = "pass.${cfg.url}";
+        DOMAIN = "https://pass.${cfg.url}";
         SIGNUPS_ALLOWED = true;
         ROCKET_ADDRESS = "127.0.0.1";
         ROCKET_PORT = 8222;
